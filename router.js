f88cffbd5ddbedd23a5ae557ac500a8a7a0e0981
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-05-04 07:14:01 -0700

    add error handling and populate user data

--- router.js
+++ router.js
@@ -1,3 +1,6 @@
+const Profile = require("./profile.js");
+
+
 //Handle HTTP route GET/ POST/
 function home(request, response){
 	if(request.url === '/'){
@@ -11,12 +14,35 @@ function home(request, response){
 
 //Handle HTTP route GET/:username
 function user(request, response){
-	let username = request.url.replace('/', '');
+	const username = request.url.replace('/', '');
 	if(username.length > 0){
 		response.writeHead(200, {'Content-Type': 'text/plain'});
 		response.write('Header\n');
-		response.write(username + '\n');
-		response.end('Footer\n');
+
+		//get json from Treehouse
+		const studentProfile = new Profile(username);
+		//on end
+		studentProfile.on("end", function(profileJSON){
+			//store JSON values we need
+			const values = {
+				avatarURL: profileJSON.gravatar_url,
+				username: profileJSON.profile_name,
+				badges: profileJSON.badges.length,
+				javascriptPoints: profileJSON.points.JavaScript
+			}
+			//simple response + end response
+			response.write(values.username + ' has ' + values.badges + '\n');
+			response.end('Footer\n');
+
+		});
+
+		//on error
+		studentProfile.on("error", function(error){
+			response.write(error.message + '\n');
+			response.end('Footer\n');
+
+		});
+
 	}
 }
 
